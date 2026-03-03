9f6c2d919219630a6285e2349fd11f5683fb427f
Thirawat <teerawattumakul@gmail.com>
2026-03-03 16:27:35 +0700

    Update 10.1.cpp

--- ENGCE117/L10/10.1.cpp
+++ ENGCE117/L10/10.1.cpp
@@ -1,1 +1,29 @@
+#include <stdio.h>
 
+long SumLoop( int );
+long SumRecur( int );
+
+int main() {
+    int n = 10;
+    
+    printf( "SumLoop(%d) = %ld\n", n, SumLoop( n ) );
+    printf( "SumRecur(%d) = %ld\n", n, SumRecur( n ) );
+
+    return 0;
+}
+
+long SumLoop( int n ) {
+    long sum = 0;
+    for( int i = 1; i <= n; i++ ) {
+        sum += i;
+    }
+    return sum;
+}
+
+long SumRecur( int n ) {
+    if ( n > 1 ) {
+        return n + SumRecur( n - 1 );
+    } else {
+        return 1;
+    }
+}
